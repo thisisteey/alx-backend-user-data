298019932ba2f7980694849a0c537efd53a49b06
thisisteey <dadataiwo82@yahoo.com>
2024-07-03 20:10:38 +0000

    Feature: Implement password validation using bcrypt for secure authentication

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Gets and returns a salted, hashed password using bcrypt"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate the plain password with the hashed password"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
