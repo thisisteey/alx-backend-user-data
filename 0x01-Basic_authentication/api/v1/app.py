2485aeec195a7074ded157ab3d452c79af85a8c6
thisisteey <dadataiwo82@yahoo.com>
2024-07-08 22:23:54 +0000

    Feature: Implement BasicAuth class and update app to use it based on AUTH_TYPE

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,12 +7,19 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+authType = getenv("AUTH_TYPE", "auth")
+if authType == "auth":
+    auth = Auth()
+if authType == "basic_auth":
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -53,11 +60,4 @@ def check_auth() -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    authType = getenv("AUTH_TYPE", "auth")
-    if authType == "auth":
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-    if authType == "basic_auth":
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
     app.run(host=host, port=port)
