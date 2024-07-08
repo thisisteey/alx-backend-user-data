7e51d6131fd09292651c95291c1c42525f3867e4
thisisteey <dadataiwo82@yahoo.com>
2024-07-08 21:33:03 +0000

    Feature: Updating request validation and authorization handling for API security

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,14 +7,12 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-authType = getenv("AUTH_TYPE", "auth")
 
 
 @app.errorhandler(404)
@@ -55,4 +53,8 @@ def check_auth() -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
+    authType = getenv("AUTH_TYPE", "auth")
+    if authType == "auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
     app.run(host=host, port=port)
