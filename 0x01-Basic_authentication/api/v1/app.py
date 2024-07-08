4836b422a0e69ffcf3c35ee988f46c3f1821cfdb
thisisteey <dadataiwo82@yahoo.com>
2024-07-08 21:24:29 +0000

    Feature: Updating request validation and authorization handling for API security

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,14 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+authType = getenv("AUTH_TYPE", "auth")
 
 
 @app.errorhandler(404)
@@ -31,6 +34,22 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """Forbidden error handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def check_auth() -> str:
+    """Sets up Flask before_request to check authorization and user auth"""
+    if auth:
+        excluded_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            authHeader = auth.authorization_header(request)
+            currUser = auth.current_user(request)
+            if authHeader is None:
+                abort(401)
+            if currUser is None:
+                abort(403)
 
 
 if __name__ == "__main__":
