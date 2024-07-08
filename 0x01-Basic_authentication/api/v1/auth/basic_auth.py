4c502848b36558c386d47412f1f34f2aded6df42
thisisteey <dadataiwo82@yahoo.com>
2024-07-08 22:04:30 +0000

    Feature: Add method to extract Base64 token from Authorization header in BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """Module for managing API basic authentication operations"""
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """Class for handling basic authentication operations"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Get & return the base64 token from the auth header for basic auth"""
+        if type(authorization_header) == str:
+            regexPtrn = r"Basic (?P<token>.+)"
+            fldMtch = re.fullmatch(regexPtrn, authorization_header.strip())
+            if fldMtch is not None:
+                return fldMtch.groups("token")[0]
+        return None
