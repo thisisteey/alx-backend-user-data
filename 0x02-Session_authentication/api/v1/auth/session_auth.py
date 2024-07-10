6f1c8bb001251f6722cbd3e51eb9418a855cecd2
thisisteey <dadataiwo82@yahoo.com>
2024-07-10 22:02:48 +0000

    Feature: Add SessionAuth class inheriting from Auth, and update app to switch auth mechanism via AUTH_TYPE

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for managing API session authentication operations"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class for handling session authentication operations"""
+    pass
