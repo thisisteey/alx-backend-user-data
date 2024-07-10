151f8851d2f153024e1374022a56613cad926b98
thisisteey <dadataiwo82@yahoo.com>
2024-07-10 23:27:43 +0000

    Feature: Add POST /auth_session/login route to handle Session authentication in Flask

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Module for handling user login and session mgt in session auth views"""
+from api.v1.views import app_views
+from typing import Tuple
+from flask import request, jsonify, abort
+from models.user import User
+import os
+
+
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
+def login() -> Tuple[str, int]:
+    """Handle user login via POST request to /api/v1/auth_session/login"""
+    userNFres = { "error": "no user found for this email" }
+    email = request.form.get("email")
+    if email is None or len(email.strip()) == 0:
+        return jsonify({ "error": "email missing" }), 400
+    password = request.form.get("password")
+    if password is None or len(password.strip()) == 0:
+        return jsonify({ "error": "password missing" }), 400
+    try:
+        usersList = User.search({"email": email})
+    except Exception:
+        return jsonify(userNFres), 404
+    if len(usersList) <= 0:
+        return jsonify(userNFres), 404
+    if usersList[0].is_valid_password(password):
+        from api.v1.app import auth
+        sessionToken = auth.create_session(getattr(usersList[0], "id"))
+        loginRes = jsonify(usersList[0].to_json())
+        loginRes.set_cookie(os.getenv("SESSION_NAME"), sessionToken)
+        return loginRes
+    return jsonify({ "error": "wrong password" }), 401
