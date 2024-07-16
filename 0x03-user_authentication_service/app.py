129fc7e885e707cb88d5dc89517c473c652e6d23
thisisteey <dadataiwo82@yahoo.com>
2024-07-16 22:19:46 +0000

    Feature: Implemented profile endpoint to handle GET /profile route with session_id cookie validation and user email response handling

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,5 +48,15 @@ def logout() -> str:
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """Gets and returns the user's profile information"""
+    currSessionID = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(currSessionID)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
