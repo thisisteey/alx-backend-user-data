d6b50b0b28b0ab02eccf081fdd760a22aa9efe25
thisisteey <dadataiwo82@yahoo.com>
2024-07-16 18:43:42 +0000

    Feature: Implement Auth.create_session method to generate and store a new session ID for a user based on their email

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,3 +44,16 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a new session for a given user"""
+        userRecord = None
+        try:
+            userRecord = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        if userRecord is None:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(userRecord.id, session_id=session_id)
+        return session_id
