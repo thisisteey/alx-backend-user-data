cd7f6f5187bfe49277e9e0368ec92c7e23990db1
thisisteey <dadataiwo82@yahoo.com>
2024-07-16 17:38:45 +0000

    Feature: Implement Auth.register_user to create a new user or raise ValueError if the user already exists

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 """Module for managing authentication-related routines"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Encrypts and return salted hash of a password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initializes an instance of Auth"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Creates and stores a new user in the database"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError(f"User {email} already exists")
